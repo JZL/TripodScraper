0da2ccb6e27f2e8f06b1d5630b6c3c2e838a0f92
Jonah <jonahmail1@gmail.com>
2017-12-06 19:31:49 -0500

    Add tripod cron bash script

--- Tripod_Cron.sh
+++ Tripod_Cron.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 #For Heron, need nodejs path bc need recent version
+cd ~/TripodScraper/TripodScraper/
+# TODO: If really fails, doesn't kill browser
+#ps aux|grep jzl|grep chrom|less -S|awk '{print $2}'|xargs kill
+
 timeout 3m ~/bin/node index.js >/dev/null
 
+if [ $? -ge 124 ]; then 
+        echo "HAD TO KILL CHECK `hostname` for zombie chrome processes"
+fi
+
+
+
+ps aux|grep chrom|grep TripodScraper
 grep -v "!!Nothing To Do!!" cron.output
+
 #if grep --quiet "Nothing To Do" cron.output; then
 #    echo exists
 #    cat cron.output
 #fi
+
+echo aa > cron.test
